4f85e15216f45eaf7fd0d85ae4590f2999152c05
hongyj9823 <hongyj9823@gmail.com>
2022-05-15 23:28:39 +0900

    ADD annualchart

--- newstock_client/src/App.js
+++ newstock_client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import HeadLogo from './components/HeadLogo.js';
 import KeywordPanel from './keywordPage/keywordPanel.js';
-import DailyNews from './keywordPage/newsOverlay.js';
 import TreemapChart from './StockPage/TreemapChart.js';
+import AnnualChart from './StockPage/annualChart.js';
+import AnnualNews from './StockPage/annualNews.js';
 import Footer from './components/Footer.js';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -17,6 +18,8 @@ function App() {
       <Routes>
         <Route path = "/" element = {<KeywordPanel/>}/>
         <Route path = "/stock" element = {<TreemapChart/>}/>
+        <Route path = "/stock/annualchart/:keyword" element = {<AnnualNews/>}></Route>  
+        <Route path =  "/stock/annualchart" element = {<AnnualChart/>}></Route>
       </Routes>
       <Footer/>
     </div>
