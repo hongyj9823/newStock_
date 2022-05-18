960e06f2cea1b2f2f458bb6d729d94cd549e7046
hongyj9823 <hongyj9823@gmail.com>
2022-05-18 16:08:13 +0900

    ADD candleCharts

--- newstock_client/src/App.js
+++ newstock_client/src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import HeadLogo from './components/HeadLogo.js';
 import KeywordPanel from './keywordPage/keywordPanel.js';
 import TreemapChart from './StockPage/TreemapChart.js';
-import AnnualChart from './StockPage/annualChart.js';
-import AnnualNews from './StockPage/annualNews.js';
+import AnnualChart from './StockPage/AnnualChart.js';
+import AnnualNews from './StockPage/AnnualNewsOverlay.js';
 import Footer from './components/Footer.js';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useNavigate } from 'react-router-dom';
 
 
 
 function App() {
-
+  let navigate = useNavigate();
   return (
     <div className = 'Frame'>
       <HeadLogo title = "newStock" />
       <Routes>
-        <Route path = "/" element = {<KeywordPanel/>}/>
-        <Route path = "/stock" element = {<TreemapChart/>}/>
-        <Route path = "/stock/annualchart/:keyword" element = {<AnnualNews/>}></Route>  
-        <Route path =  "/stock/annualchart" element = {<AnnualChart/>}></Route>
-      </Routes>
+        <Route path = "/" element= {<KeywordPanel/>}/>
+        <Route path = "/stock" element = {<TreemapChart />}/>
+        <Route path = "/stock/annualchart/pastnews" element = {<AnnualNews/>}></Route>  
+          <Route path =  "/stock/annualchart" element = {<AnnualChart />}></Route> 
+      </Routes> 
       <Footer/>
     </div>
   );
